df43f711a1ca9eaae90b4ad9b3aa6d793ff7b656
Pribess <heewon6752@gmail.com>
2023-01-27 17:19:54 +0900

    impelemnt udp loop

--- src/isr.c
+++ src/isr.c
@@ -10,8 +10,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <errno.h>
 #include <locale.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/select.h>
+
+#include "query.h"
+
+void udp_loop();
 
 int main(int argc, char *argv[]) {
 	setlocale(LC_ALL, "");
@@ -20,7 +29,7 @@ int main(int argc, char *argv[]) {
 	argv++;
 
 	if (argc < 1) {
-		printf("usage: isr [-v] file\n");
+		printf("usage: isr [-v] file dns\n");
 		return 1;
 	}
 
@@ -29,11 +38,41 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 
-	FILE *fp;
-	if ((fp = fopen(*argv, "r")) == NULL) {
-		printf("isr: %s: %s\n", *argv, strerror(errno));
-		return 1;
-	}
+	udp_loop();
 
 	return 0;
+}
+
+void udp_loop() {
+	printf("UDP server initializing...\n");
+
+	int sockfd;
+
+	struct sockaddr_in addr;
+	struct sockaddr_in clientaddr;
+	socklen_t addrlen = sizeof(struct sockaddr_in);
+
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons(53);
+
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		perror("isr: ");
+		exit(-1);
+	}
+
+	if (bind(sockfd, (struct sockaddr *)&addr, addrlen) < 0) {
+		perror("isr: ");
+		exit(-1);
+	}
+
+	printf("UDP server successfully initialized!\n");
+
+	unsigned char *buf[512];
+
+	while (true) {
+		int cnt = recvfrom(sockfd, buf, sizeof(buf), 0, (struct sockaddr *)&clientaddr, &addrlen);
+		
+		// handling request with packet in buf and packet length in cnt
+	}
 }
