8424336a66130fcd7b61ff0714643992b99db740
Janghyub Seo <jhyub06@gmail.com>
2023-02-08 01:32:47 +0900

    draft: rdata.js: IPV4, CharacterString

--- src/script/js/rdata.js
+++ src/script/js/rdata.js
@@ -1,3 +1,15 @@
+function CharacterString(str) {
+    this.str = str;
+}
+
+CharacterString.prototype.toUint8Array = function() {
+    var ret = new Uint8Array(this.str.length + 1); // TODO : Make sure of ascii string?
+    var encoder = new TextEncoder();
+    ret.set([this.str.length], 0);
+    ret.set(encoder.encode(this.str), 1);
+    return ret;
+}
+
 function DomainName(str) {
     if (str.slice(-1) == '.') {
         this.str = str.slice(0, -1); /* Remove trailing dot for convenience */
@@ -10,7 +22,7 @@ DomainName.prototype.toUint8Array = function() {
     var ret = new Uint8Array(this.str.length + 2);
     var cursor = 0;
     var encoder = new TextEncoder();
-    var labels = this.str.split(".")
+    var labels = this.str.split(".");
     for (var i in labels) {
         ret.set([labels[i].length], cursor);
         ret.set(encoder.encode(labels[i]), cursor+1);
@@ -26,7 +38,13 @@ function IPV4(ip) {
 }
 
 IPV4.prototype.toUint8Array = function() {
-    // TODO
+    // TODO: implement validating & error throwing
+    var ret = new Uint8Array(4);
+    var octets = this.ip.split(".");
+    for (var i in octets) {
+        ret.set([parseInt(octets[i])], i);
+    }
+    return ret;
 };
 
 function RawRData(uint8Array) {
