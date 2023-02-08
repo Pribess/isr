435890c8e2e527b331f9e76c5850360daa437437
Janghyub Seo <jhyub06@gmail.com>
2023-02-08 10:49:34 +0900

    implement evaluator

--- src/script/engine.c
+++ src/script/engine.c
@@ -22,3 +22,67 @@ duk_bool_t isr_push_question(duk_context *ctx, struct question *question) {
     duk_push_uint(ctx, question->qclass);
     duk_def_prop(ctx, idx, DUK_DEFPROP_HAVE_VALUE | DUK_DEFPROP_CLEAR_WEC);
 }
+
+struct eval_result *isr_evaluate(duk_context *ctx, struct question *question) {
+    int stacksize = 0;
+    /* Go reverse-alphabetic order, so we could look up from idx -2 */
+    duk_get_global_string(ctx, "Forward");
+    duk_get_global_string(ctx, "Answer");
+    stacksize += 2;
+
+    duk_get_global_string(ctx, "resolve");
+    isr_push_question(ctx, question);
+    duk_int_t res = duk_pcall(ctx, 1);
+    stacksize += 1;
+    if (res != DUK_EXEC_SUCCESS) goto error;
+
+    if (duk_instanceof(ctx, -1, -2)) {
+        duk_get_prop_string(ctx, -1, "rdata"); /* answer.rdata */
+        duk_get_prop_string(ctx, -2, "type"); /* answer.type */
+        duk_push_string(ctx, "toUint8Array");
+        duk_int_t res = duk_pcall_prop(ctx, -3, 0); /* answer.rdata.toUint8Array() */
+        stacksize += 3;
+        if (res != DUK_EXEC_SUCCESS) goto error;
+
+        struct eval_result_answer *ans = malloc(sizeof(struct eval_result_answer));
+        ans->type = duk_get_number(ctx, -2);
+        ans->rdata = duk_get_buffer_data(ctx, -1, &ans->rdlength);
+
+        struct eval_result *rst = malloc(sizeof(struct eval_result));
+        rst->type = ANSWER;
+        rst->value.answer = ans;
+        
+        duk_pop_n(ctx, stacksize);
+        return rst;
+    } else if (duk_instanceof(ctx, -1, -3)) {
+        duk_get_prop_string(ctx, -1, "ip"); /* forward.ip */
+        stacksize += 1;
+
+        struct eval_result_forward *fwd = malloc(sizeof(struct eval_result_forward));
+        fwd->ip = duk_get_string(ctx, -1);
+
+        struct eval_result *rst = malloc(sizeof(struct eval_result));
+        rst->type = FORWARD;
+        rst->value.forward = fwd;
+
+        duk_pop_n(ctx, stacksize);
+        return rst;
+    } else {
+        printf("isr: evaluate: Expected value to be Answer or Forward, but value was \'%s\'\n", duk_safe_to_string(ctx, -1));
+
+        struct eval_result *rst = malloc(sizeof(struct eval_result));
+        rst->type = FALLBACK;
+
+        duk_pop_n(ctx, stacksize);
+        return rst;
+    }
+
+error:
+    printf("isr: evaluate: %s\n", duk_safe_to_string(ctx, -1)); // TODO: think of a better error msg?
+
+    struct eval_result *rst = malloc(sizeof(struct eval_result));
+    rst->type = FALLBACK;
+
+    duk_pop_n(ctx, stacksize);
+    return rst;
+}
--- src/script/engine.h
+++ src/script/engine.h
@@ -14,4 +14,24 @@
 
 duk_bool_t isr_push_question(duk_context *ctx, struct question *question);
 
+struct eval_result_answer {
+	uint16_t type;
+	uint16_t rdlength;
+	unsigned char *rdata;
+};
+
+struct eval_result_forward {
+	char *ip;	
+};
+
+struct eval_result {
+	enum { ANSWER, FORWARD, FALLBACK } type;
+	union {
+		struct eval_result_answer *answer;
+		struct eval_result_forward *forward;
+	} value;
+};
+
+struct eval_result *isr_evaluate(duk_context *ctx, struct question *question);
+
 #endif
